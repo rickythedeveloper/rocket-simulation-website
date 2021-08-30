93efa50b6717469b754dd84e4d090b134afd58ef
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-08-30 20:43:52 +0100

    #6: KeyboardStates class

---
+++ src/utils/KeyboardStates.ts
@@ -0,0 +1,30 @@
+enum KeyState {
+	up,
+	down,
+}
+
+export default class KeyboardStates {
+	keyStates: { [key: string]: KeyState } = {};
+
+	addKey(key: string, downCallback?: (event: KeyboardEvent) => void, upCallback?: (event: KeyboardEvent) =>void) {
+		if (key in Object.keys(this.keyStates)) return;
+		this.keyStates[key] = KeyState.up;
+
+		document.addEventListener('keydown', (event: KeyboardEvent) => {
+			if (event.key === key) {
+				if (this.keyStates[key] !== KeyState.down) {
+					this.keyStates[key] = KeyState.down;
+					if (downCallback) downCallback(event);
+				}
+			}
+		});
+		document.addEventListener('keyup', (event: KeyboardEvent) => {
+			if (event.key === key) {
+				if (this.keyStates[key] !== KeyState.up) {
+					this.keyStates[key] = KeyState.up;
+					if (upCallback) upCallback(event);
+				}
+			}
+		});
+	}
+}
