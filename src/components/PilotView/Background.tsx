b7cafbef55dd05f4500bd0a216a5539fe400bff4
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-08-31 15:12:49 +0100

    #12: Add land

--- src/components/PilotView/Background.tsx
+++ src/components/PilotView/Background.tsx
@@ -4,24 +4,56 @@ interface Props {
 	cameraHeight: number;
 	style?: CSSProperties;
 }
-interface State {}
+interface State {
+	lightStrength: number;
+}
+
+const DEFAULT_STATE: State = {
+	lightStrength: 1,
+};
 
 const MAX_HEIGHT_COLOR = 20000;
 export default class Background extends React.Component<Props, State> {
-	render() {
-		let lightStrength: number;
-		if (this.props.cameraHeight < 0) lightStrength = 1;
-		else if (this.props.cameraHeight > MAX_HEIGHT_COLOR) lightStrength = 0;
-		else lightStrength = 1 - this.props.cameraHeight / MAX_HEIGHT_COLOR;
 
+	static getDerivedStateFromProps(props: Props) {
+		let lightStrength: number;
+		if (props.cameraHeight < 0) lightStrength = 1;
+		else if (props.cameraHeight > MAX_HEIGHT_COLOR) lightStrength = 0;
+		else lightStrength = 1 - props.cameraHeight / MAX_HEIGHT_COLOR;
+		return {
+			lightStrength: lightStrength,
+		};
+	}
+
+	constructor(props: Props) {
+		super(props);
+		this.state = DEFAULT_STATE;
+	}
+
+	render() {
 		const containerStyle: CSSProperties = {
-			backgroundColor: `rgba(${lightStrength * 130}, ${lightStrength * 200}, ${lightStrength * 255}, 1.0)`,
+			backgroundColor: `rgba(
+				${this.state.lightStrength * 130}, 
+				${this.state.lightStrength * 200}, 
+				${this.state.lightStrength * 255}, 
+				1.0)
+			`,
 			...this.props.style,
+		};
+
+		const landTop = 400 + this.props.cameraHeight; // TODO: Change 400 to wherever the rocket is
+		const landStyle: CSSProperties = {
+			position: 'absolute',
+			backgroundColor: '#3f3',
+			top: `${landTop}px`,
+			width: '100%',
+			height: '100px',
+			borderTop: '5px solid black',
 		};
 
 		return (
 			<div style={containerStyle}>
-				hello
+				<div style={landStyle}></div>
 			</div>
 		);
 	}
