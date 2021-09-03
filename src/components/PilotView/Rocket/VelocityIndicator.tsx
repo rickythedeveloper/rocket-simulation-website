ae42fcb01be0f753b3eac748cd92bdbe671db0a1
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-09-03 18:50:04 +0100

    #36: Use svg for velocity indicator arrow. Fixes #36.

--- src/components/PilotView/Rocket/VelocityIndicator.tsx
+++ src/components/PilotView/Rocket/VelocityIndicator.tsx
@@ -1,5 +1,4 @@
 import React, { CSSProperties } from 'react';
-import rightArrowImage from '../../../assets/rightArrow.png';
 
 interface Props {
 	arrowInnerRadius: number;
@@ -8,24 +7,34 @@ interface Props {
 	style?: CSSProperties;
 }
 
-const INDICATOR_WIDTH = 10;
+const ARROW_MAX_WIDTH = 20;
 export default class VelocityIndicator extends React.Component<Props> {
 	render() {
 		const containerStyle: CSSProperties = {
 			width: `${this.props.arrowOuterRadius * 2}px`,
-			height: `${INDICATOR_WIDTH}px`,
+			height: `${ARROW_MAX_WIDTH}px`,
 			transform: `rotateZ(${-this.props.angularPosition * 180 / Math.PI}deg)`,
 			...this.props.style,
 		};
+		const arrowLength = this.props.arrowOuterRadius - this.props.arrowInnerRadius;
 		const arrowStyle: CSSProperties = {
-			width: `${this.props.arrowOuterRadius - this.props.arrowInnerRadius}px`,
-			height: `${INDICATOR_WIDTH}px`,
+			width: `${arrowLength}px`,
+			height: `${ARROW_MAX_WIDTH}px`,
 			left: `${this.props.arrowOuterRadius + this.props.arrowInnerRadius}px`,
 			position: 'absolute',
 		};
+		const arrowColor = '#fff'; // TODO #33 dynamically change color
+
 		return (
 			<div style={containerStyle}>
-				<img src={rightArrowImage} style={arrowStyle}/>
+				{/* eslint-disable max-len */}
+				<svg style={arrowStyle} viewBox="0 0 756 273" version="1.1" xmlns="http://www.w3.org/2000/svg">
+					<title>arrow</title>
+					<g id="arrow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+						<path d="M36.5,135.5 L719.5,135.5 M718.847561,135.5 L532.152439,243.5 M718.847561,135.5 L532.152439,28.5" id="Combined-Shape" stroke={arrowColor} strokeWidth="50"></path>
+					</g>
+				</svg>
+				{/* eslint-enable max-len */}
 			</div>
 		);
 	}
